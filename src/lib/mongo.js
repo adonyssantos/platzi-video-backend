b0be0efe344834d7000914191ec357a850256f18
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-18 13:41:42 -0400

    Add services connection with MongoDB

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -5,24 +5,27 @@ const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const DB_NAME = encodeURIComponent(config.dbName);
 
-const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`;
 
 class MongoLib {
   constructor() {
-    new MongoClient(MONGO_URI, { userNewUrlParser: true });
+    this.client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     this.dbName = DB_NAME;
   }
 
   connect() {
     if (!MongoLib.connection) {
       MongoLib.connection = new Promise((resolve, reject) => {
-        this.MongoClient.connect(error => {
+        this.client.connect(error => {
           if (error) {
             reject(error);
           }
 
           console.log('Connect successfully to mongo');
-          resolve(this.client.dbHost(this.dbName));
+          resolve(this.client.db(this.dbName));
         });
       });
     }
